325f92d03356de79fbf7784c071ce6cdf3a3b01e
Aqib Javed <aqibbutt3078@gmail.com>
2019-11-19 18:10:50 +0500

    Initial Commit

---
+++ URL-Scanner/src/main/java/com/example/demo/controller/URLController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class URLController {
+
+	@PostMapping("/scan")
+	public ResponseEntity<?> scanUrl(@RequestBody List<String> urls) {
+		return null;
+	}
+}
---
+++ URL-Scanner/src/main/java/com/example/demo/service/URLServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+public class URLServiceImpl implements URLService{
+
+	@Override
+	public void scanService(List<String> urls) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

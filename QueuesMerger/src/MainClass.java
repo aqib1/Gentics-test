429aab90d88ce54e8504143c4d8bd392a8ecedf7
Aqib Javed <aqibbutt3078@gmail.com>
2019-11-24 21:47:23 +0500

    QueueMerger algorithm

--- QueuesMerger/src/MainClass.java
+++ QueuesMerger/src/MainClass.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,9 +9,16 @@ public class MainClass {
 		// 2, 9, 15
 		// 3, 6, 8
 		// 9, 10, 12
-		Queue<Integer> queue1 = new LinkedList<>(Arrays.asList(2, 9, 15));
-		System.out.println(queue1.poll());
-		System.out.println(queue1);
+		Queue<Integer> queue1 = new LinkedList<>(Arrays.asList(2, 9, 15, 22 , 38));
+		Queue<Integer> queue2 = new LinkedList<>(Arrays.asList(3, 6, 8, 21, 23));
+		Queue<Integer> queue3 = new LinkedList<>(Arrays.asList(9, 10, 12, 25, 27));
+		HashMap<Integer, Queue<Integer>> queueMap = new HashMap<>();
+		queueMap.put(0, queue1);
+		queueMap.put(1, queue2);
+		queueMap.put(2, queue3);
+		QueueMerger merger = new QueueMerger();
+		System.out.println(merger.merger(queueMap));
+		
 	}
 
 }
--- QueuesMerger/src/QueueMerger.java
+++ QueuesMerger/src/QueueMerger.java
@@ -1,9 +1,66 @@
-import java.util.List;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map.Entry;
 import java.util.Queue;
+import java.util.Set;
 
 public class QueueMerger {
 
-	public Queue<Integer> merger(List<Queue<Integer>> queues) {
-		return null;
+	public Queue<Integer> merger(HashMap<Integer, Queue<Integer>> queues) {
+		Queue<Integer> merger = new LinkedList<Integer>();
+		HashMap<Integer, Integer> minMap = new HashMap<>();
+		fillWithZeroIndexValues(minMap, queues);
+		do {
+			fillMinValues(minMap, merger, queues);
+
+		} while (!queues.isEmpty());
+		for (int x = 0; x <= minMap.size(); x++) {
+			int[] val = getMinValueWithIndex(minMap);
+			merger.add(val[1]);
+		}
+		return merger;
+	}
+
+	private void fillMinValues(HashMap<Integer, Integer> minMap, Queue<Integer> merger,
+			HashMap<Integer, Queue<Integer>> queues) {
+		if (queues.isEmpty())
+			return;
+		emptyQueueIndex(queues);
+		int[] val = getMinValueWithIndex(minMap);
+		merger.add(val[1]);
+		try {
+			minMap.put(val[0], queues.get(val[0]).poll());
+			return;
+		} catch (NullPointerException e) {
+			fillMinValues(minMap, merger, queues);
+		}
+
+	}
+
+	private int[] getMinValueWithIndex(HashMap<Integer, Integer> minMap) {
+		int[] minVal = new int[2];
+		Entry<Integer, Integer> entry = minMap.entrySet().stream().min(Comparator.comparing(Entry::getValue))
+				.orElse(null);
+		if (entry == null)
+			return minVal;
+		minVal[0] = entry.getKey();
+		minVal[1] = entry.getValue();
+		minMap.remove(minVal[0]);
+		return minVal;
+	}
+
+	private void fillWithZeroIndexValues(HashMap<Integer, Integer> minMap, HashMap<Integer, Queue<Integer>> queues) {
+		Set<Integer> keys = queues.keySet();
+		for (int x : keys) {
+			minMap.put(x, queues.get(x).poll());
+		}
+	}
+
+	private void emptyQueueIndex(HashMap<Integer, Queue<Integer>> queues) {
+		Entry<Integer, Queue<Integer>> entry = queues.entrySet().parallelStream().filter(x -> x.getValue().isEmpty())
+				.findFirst().orElse(null);
+		if (entry != null)
+			queues.remove(entry.getKey());
 	}
 }
